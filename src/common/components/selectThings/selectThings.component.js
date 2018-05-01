a8f7bf92e9576d2c5054bd7d8871b52d97e370a5
dusty <ddoris@parkbenchsolutions.com>
2018-05-01 14:55:54 -0700

    expand numbers for adding/removing from group

--- src/common/components/selectThings/selectThings.component.js
+++ src/common/components/selectThings/selectThings.component.js
@@ -2,18 +2,44 @@
   angular.module('odin.common').component('selectThings', {
     templateUrl: 'common/components/selectThings/selectThings.component.html',
     controller: Controller,
-    bindings: { available: '=', selected: '=', property: '@', default: '@' }
+    bindings: {
+      available: '=',
+      selected: '=',
+      orderBy: '<',
+      property: '@',
+      availableTitle: '@',
+      selectedTitle: '@'
+    }
   })
 
   function Controller() {
     var ctrl = this
+    ctrl.$onChanges = onChanges
     ctrl.add = add
+    ctrl.addAll = addAll
     ctrl.remove = remove
+    ctrl.removeAll = removeAll
 
-    ctrl.$onInit = function() {
-      ctrl.orderBy = ctrl.default
-        ? ['-' + ctrl.default, ctrl.property]
-        : ctrl.property
+    function onChanges(changes) {
+      if (changes.orderBy) {
+        ctrl.orderBy = changes.orderBy.currentValue || ctrl.property
+      }
+    }
+
+    function addAll() {
+      for (var i = 0; i < ctrl.available.length; i++) {
+        ctrl.selected.push(ctrl.available[i])
+        ctrl.available.splice(i, 1)
+        i--
+      }
+    }
+
+    function removeAll() {
+      for (var i = 0; i < ctrl.selected.length; i++) {
+        ctrl.available.push(ctrl.selected[i])
+        ctrl.selected.splice(i, 1)
+        i--
+      }
     }
 
     function add(thing) {
--- src/group/components/numbers/numbers.component.js
+++ src/group/components/numbers/numbers.component.js
@@ -9,23 +9,20 @@
     Alert,
     ServiceProviderNumberService,
     GroupNumberService,
-    $q
+    NumberService
   ) {
     var ctrl = this
     ctrl.$onInit = onInit
-    ctrl.edit = edit
-    ctrl.assign = assign
     ctrl.serviceProviderId = $routeParams.serviceProviderId
     ctrl.groupId = $routeParams.groupId
+    ctrl.add = add
+    ctrl.edit = edit
     ctrl.toggle = toggle
-    ctrl.filterStatus = filterStatus
 
     function onInit() {
       ctrl.loading = true
       loadNumbers()
-        .catch(function(error) {
-          Alert.notify.danger(error)
-        })
+        .catch(Alert.notify.danger)
         .finally(function() {
           ctrl.loading = false
         })
@@ -36,137 +33,84 @@
         ctrl.serviceProviderId,
         ctrl.groupId
       ).then(function(data) {
-        // console.log('numbers', data);
+        console.log('numbers', data)
         ctrl.numbers = data
-        return data
       })
     }
 
     function loadAvailableNumbers() {
-      Alert.spinner.open()
       return ServiceProviderNumberService.index(
         ctrl.serviceProviderId,
         'available'
       )
-        .then(function(data) {
-          // console.log('availableNumbers', data);
-          ctrl.availableNumbers = data
-          return data
-        })
-        .catch(function(error) {
-          Alert.notify.danger(error)
-          return $q.reject(error)
-        })
-        .finally(function() {
-          Alert.spinner.close()
-        })
     }
 
-    function numberName(number) {
-      if (!number) return
-      return number.max ? number.min + ' - ' + number.max : number.min
+    function add() {
+      Alert.spinner.open()
+      loadAvailableNumbers()
+        .then(function(numbers) {
+          ctrl.addNumbers = {
+            unassigned: NumberService.expand(numbers),
+            assign: []
+          }
+          Alert.modal.open('groupNumbersAddModal', function onSave(close) {
+            assignNumbers(ctrl.addNumbers.assign, close)
+          })
+        })
+        .catch(Alert.notify.danger)
+        .finally(Alert.spinner.close)
     }
 
     function edit(number) {
-      ctrl.editNumber = angular.copy(number)
-      ctrl.modalTitle = numberName(number)
-      Alert.modal.open(
-        'edit-groupNumbers',
-        function onSave(close) {
-          if (!ctrl.editNumber.activated && number.activated) {
-            var message = 'Are you sure you want to deactivate this DN?'
-            Alert.confirm.open(message).then(function() {
-              update(ctrl.editNumber, close)
-            })
-          } else {
-            update(ctrl.editNumber, close)
-          }
-        },
-        function onDelete(close) {
-          unassign(number, close)
-        }
-      )
+      if (number.assigned) return
+      ctrl.editNumbers = {
+        assigned: NumberService.expand(number),
+        unassign: []
+      }
+      Alert.modal.open('groupNumbersEditModal', function onSave(close) {
+        unassignNumbers(ctrl.editNumbers.unassign, close)
+      })
     }
 
-    function update(number, callback) {
-      Alert.spinner.open()
+    function toggle(number) {
+      number.isLoading = true
       GroupNumberService.update(ctrl.serviceProviderId, ctrl.groupId, [number])
-        .then(loadNumbers)
         .then(function() {
-          Alert.notify.success('Number Updated')
-          callback()
+          if (number.activated) {
+            Alert.notify.success('Number Activated')
+          } else {
+            Alert.notify.warning('Number Deactivated')
+          }
         })
         .catch(function(error) {
-          console.log('error', error)
           Alert.notify.danger(error)
+          number.activated = !number.activated
         })
-        .finally(function() {
-          Alert.spinner.close()
-        })
-    }
-
-    function assign() {
-      loadAvailableNumbers().then(function() {
-        Alert.modal.open('assign-GroupNumbers', function onSave(close) {
-          var toAssign = _.filter(ctrl.availableNumbers, 'groupId')
-          assignNumbers(toAssign, close)
-        })
-      })
+        .finally(loadNumbers)
     }
 
     function assignNumbers(numbers, callback) {
       Alert.spinner.open()
-      console.log('assigning', numbers)
       GroupNumberService.assign(ctrl.serviceProviderId, ctrl.groupId, numbers)
         .then(loadNumbers)
         .then(function() {
           Alert.notify.success('Numbers Assigned')
           callback()
         })
-        .catch(function(error) {
-          console.log('error', error.data)
-          Alert.notify.danger(error)
+        .catch(Alert.notify.danger)
+        .finally(Alert.spinner.close)
+    }
+
+    function unassignNumbers(numbers, callback) {
+      Alert.spinner.open()
+      GroupNumberService.unassign(ctrl.serviceProviderId, ctrl.groupId, numbers)
+        .then(loadNumbers)
+        .then(function() {
+          Alert.notify.success('Numbers Unassigned')
+          callback()
         })
-        .finally(function() {
-          Alert.spinner.close()
-        })
-    }
-
-    function unassign(number, callback) {
-      var message =
-        'Are you sure you want to release this number from the Group?'
-      Alert.confirm.open(message).then(function() {
-        Alert.spinner.open()
-        GroupNumberService.unassign(ctrl.serviceProviderId, ctrl.groupId, [
-          number
-        ])
-          .then(loadNumbers)
-          .then(function() {
-            Alert.notify.success('Number Released')
-            callback()
-          })
-          .catch(function(error) {
-            Alert.notify.danger(error)
-          })
-          .finally(function() {
-            Alert.spinner.close()
-          })
-      })
-    }
-
-    function toggle(number) {
-      number.groupId = number.groupId ? null : ctrl.groupId
-    }
-
-    function filterStatus(item) {
-      ctrl.status = ctrl.status || 'all'
-      if (ctrl.status === 'activated') {
-        return item.activated
-      } else if (ctrl.status === 'assigned') {
-        return item.assigned
-      } else {
-        return true
-      }
+        .catch(Alert.notify.danger)
+        .finally(Alert.spinner.close)
     }
   }
 })()
