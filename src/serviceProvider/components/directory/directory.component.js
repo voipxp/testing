c496788d208a70398510ace8dbd204979c325471
dusty <ddoris@parkbenchsolutions.co>
2018-06-29 12:57:49 -0700

    add sort and download options to enterprise directory

--- src/serviceProvider/components/directory/directory.component.js
+++ src/serviceProvider/components/directory/directory.component.js
@@ -10,12 +10,46 @@
   function Controller(
     Alert,
     ServiceProviderPhoneDirectoryService,
-    $routeParams
+    $routeParams,
+    CsvService,
+    DownloadService
   ) {
     var ctrl = this
     ctrl.$onInit = onInit
     ctrl.onPagination = onPagination
     ctrl.serviceProviderId = $routeParams.serviceProviderId
+    ctrl.download = download
+
+    ctrl.columns = [
+      {
+        key: 'groupId',
+        label: 'Group ID'
+      },
+      {
+        key: 'userId',
+        label: 'User ID'
+      },
+      {
+        key: 'lastName',
+        label: 'Last Name'
+      },
+      {
+        key: 'firstName',
+        label: 'First Name'
+      },
+      {
+        key: 'number',
+        label: 'Phone Number'
+      },
+      {
+        key: 'extension',
+        label: 'Extension'
+      },
+      {
+        key: 'department',
+        label: 'Department'
+      }
+    ]
 
     function onPagination(event) {
       ctrl.pager = event.pager
@@ -29,6 +63,19 @@
         })
         .finally(function() {
           ctrl.loading = false
+        })
+    }
+
+    function download() {
+      var now = new Date()
+      var filename =
+        ctrl.serviceProviderId + '-directory-' + now.toJSON() + '.csv'
+      CsvService.export(ctrl.users)
+        .then(function(data) {
+          DownloadService.download(data, filename)
+        })
+        .catch(function(error) {
+          Alert.notify.danger(error)
         })
     }
 
