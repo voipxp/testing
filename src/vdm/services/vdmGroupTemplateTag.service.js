0bf5c3158930eeb6a6c79fbaae7c4fe761658ca0
dusty <ddoris@parkbenchsolutions.co>
2018-08-07 15:16:21 -0700

    fixes for support-vocus#83

--- src/vdm/services/vdmGroupTemplateTag.service.js
+++ src/vdm/services/vdmGroupTemplateTag.service.js
@@ -4,7 +4,8 @@
   function Service($http, Route) {
     var service = {
       index: index,
-      update: update
+      update: update,
+      store: store
     }
     return service
 
@@ -24,6 +25,14 @@
     function index(serviceProviderId, groupId, templateId) {
       return $http
         .get(url(serviceProviderId, groupId, templateId))
+        .then(function(response) {
+          return response.data
+        })
+    }
+
+    function store(serviceProviderId, groupId, templateId, tag) {
+      return $http
+        .post(url(serviceProviderId, groupId, templateId), tag)
         .then(function(response) {
           return response.data
         })
