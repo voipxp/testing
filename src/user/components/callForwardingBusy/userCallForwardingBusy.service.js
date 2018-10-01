5a9c71ed7896ebe14c5657ee951fcf812ee50436
dusty <ddoris@parkbenchsolutions.co>
2018-10-01 12:52:35 -0700

    add Call Forwarding Busy V2 Routes

--- src/user/components/callForwardingBusy/userCallForwardingBusy.service.js
+++ src/user/components/callForwardingBusy/userCallForwardingBusy.service.js
@@ -2,7 +2,7 @@
   angular.module('odin.user').factory('UserCallForwardingBusyService', Service)
 
   function Service($http, Route, CacheFactory) {
-    var url = Route.api('/services/users/callforwardingbusy')
+    var url = Route.api2('/users/call-forwarding-busy')
     var service = { show: show, update: update, bulk: bulk }
     service.options = {
       outgoingDNorSIPURI: { minimum: 1, maximum: 161 }
@@ -11,20 +11,22 @@
     return service
 
     function show(userId) {
-      return $http.get(url(userId), { cache: cache }).then(function(response) {
-        return response.data
-      })
+      return $http
+        .get(url(), { params: { userId: userId }, cache: cache })
+        .then(function(response) {
+          return response.data
+        })
     }
 
     function update(userId, obj) {
-      return $http.put(url(userId), obj).then(function(response) {
+      return $http.put(url(), obj).then(function(response) {
         cache.removeAll()
         return response.data
       })
     }
 
     function bulk(data) {
-      return $http.put(url(), data).then(function(response) {
+      return $http.put(url('bulk'), data).then(function(response) {
         cache.removeAll()
         return response.data
       })
