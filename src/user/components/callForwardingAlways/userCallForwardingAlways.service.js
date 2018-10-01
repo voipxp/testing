5b8bd1b1109c98f2dc29dd2d2b3bcc93004e0050
dusty <ddoris@parkbenchsolutions.co>
2018-10-01 12:28:59 -0700

    Add user Call Forwarding Always V2 routes

--- src/user/components/callForwardingAlways/userCallForwardingAlways.service.js
+++ src/user/components/callForwardingAlways/userCallForwardingAlways.service.js
@@ -4,7 +4,7 @@
     .factory('UserCallForwardingAlwaysService', Service)
 
   function Service($http, Route, CacheFactory) {
-    var url = Route.api('/services/users/callforwardingalways')
+    var url = Route.api2('/users/call-forwarding-always')
     var service = { show: show, update: update, bulk: bulk }
     service.options = {
       outgoingDNorSIPURI: { minimum: 1, maximum: 161 }
@@ -13,20 +13,22 @@
     return service
 
     function show(userId) {
-      return $http.get(url(userId), { cache: cache }).then(function(response) {
-        return response.data
-      })
+      return $http
+        .get(url(), { params: { userId: userId }, cache: cache })
+        .then(function(response) {
+          return response.data
+        })
     }
 
     function update(userId, obj) {
-      return $http.put(url(userId), obj).then(function(response) {
+      return $http.put(url(), obj).then(function(response) {
         cache.removeAll()
         return response.data
       })
     }
 
     function bulk(data) {
-      return $http.put(url(), data).then(function(response) {
+      return $http.put(url('bulk'), data).then(function(response) {
         cache.removeAll()
         return response.data
       })
