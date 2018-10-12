bf379c06910b144824fdf48e0fb86f227edd77f8
Johnny Cash <developer@parkbenchsolutions.com>
2018-10-11 20:58:31 -0400

    convert service provider phone directory

--- src/serviceProvider/services/serviceProviderPhoneDirectory.service.js
+++ src/serviceProvider/services/serviceProviderPhoneDirectory.service.js
@@ -4,21 +4,16 @@
     .factory('ServiceProviderPhoneDirectoryService', Service)
 
   function Service($http, Route) {
+    var url = Route.api2('/service-providers/phone-directory')
     var service = { show: show }
     return service
 
-    function url(serviceProviderId) {
-      return Route.api(
-        'serviceproviders',
-        serviceProviderId,
-        'phonedirectory'
-      )()
-    }
-
     function show(serviceProviderId) {
-      return $http.get(url(serviceProviderId)).then(function(response) {
-        return response.data
-      })
+      return $http
+        .get(url(), { params: { serviceProviderId: serviceProviderId } })
+        .then(function(response) {
+          return response.data
+        })
     }
   }
 })()
