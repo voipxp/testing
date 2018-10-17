b147964ef60fc9a6399a778bfd9d3f58f52b2d85
dusty <ddoris@parkbenchsolutions.co>
2018-10-17 16:50:37 -0700

    API v2 for user report

--- src/group/services/userReport.service.js
+++ src/group/services/userReport.service.js
@@ -3,46 +3,17 @@
 
   function UserReportService($http, Route) {
     var service = { index: index, show: show }
+    var url = Route.api2('/groups/reports/users')
     return service
-
-    function url(serviceProviderId, groupId, userId) {
-      if (serviceProviderId && groupId) {
-        return Route.api()(
-          'serviceproviders',
-          serviceProviderId,
-          'groups',
-          groupId,
-          'reports',
-          'users',
-          userId
-        )
-      } else if (serviceProviderId && !groupId) {
-        return Route.api()(
-          'serviceproviders',
-          serviceProviderId,
-          'reports',
-          'users',
-          userId
-        )
-      } else {
-        throw new Error('Missing Service Provider Id or Group Id')
-      }
-    }
 
     function index(serviceProviderId, groupId) {
       return $http
-        .get(url(serviceProviderId, groupId))
-        .then(function(response) {
-          return response.data
-        })
+        .get(url(), { params: { serviceProviderId, groupId } })
+        .then(res => res.data)
     }
 
-    function show(serviceProviderId, groupId, userId) {
-      return $http
-        .get(url(serviceProviderId, groupId, userId))
-        .then(function(response) {
-          return response.data
-        })
+    function show(userId) {
+      return $http.get(url(), { params: { userId } }).then(res => res.data)
     }
   }
 })()
