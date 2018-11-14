127d27f4934156118ba003bed3d35fe958f79e20
dusty <ddoris@parkbenchsolutions.com>
2018-11-14 09:44:04 -0500

    initial dnd

--- src/UI/pbsInputFile/pbsInputFile.directive.js
+++ src/UI/pbsInputFile/pbsInputFile.directive.js
@@ -1,21 +1,43 @@
+/* globals FileReader */
 ;(function() {
   angular.module('odin.UI').directive('pbsInputFile', Directive)
 
   function Directive() {
     return {
       restrict: 'E',
+      scope: { onUpload: '&', mode: '@' },
       templateUrl: 'UI/pbsInputFile/pbsInputFile.directive.html',
-      scope: {},
-      controller: function() {
-        this.callback = function(file) {
-          this.name = file.name
-          this.onUpload({ file: file })
+      link: function(scope, element) {
+        const input = element.find('input')[0]
+        input.addEventListener('change', event => {
+          handleFile(event.target.files[0])
+        })
+        const dropzone = element[0]
+        dropzone.addEventListener('dragover', event => event.preventDefault())
+        dropzone.addEventListener('dragenter', event => event.preventDefault())
+        dropzone.addEventListener('drop', event => {
+          event.preventDefault()
+          handleFile(event.dataTransfer.files[0])
+        })
+
+        function handleFile(file) {
+          const reader = new FileReader()
+          reader.onload = event => {
+            file.content = event.target.result
+            scope.name = file.name
+            scope.onUpload({ file })
+          }
+          switch (scope.mode) {
+            case 'text':
+              return reader.readAsText(file)
+            case 'array-buffer':
+              return reader.readAsArrayBuffer(file)
+            case 'binary-string':
+              return reader.readAsBinaryString(file)
+            default:
+              return reader.readAsDataURL(file)
+          }
         }
-      },
-      controllerAs: '$ctrl',
-      bindToController: {
-        onUpload: '&',
-        mode: '@'
       }
     }
   }
