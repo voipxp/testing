5092391f415c3c8289333285b63d9137d6cc7a3e
Johnny Cash <developer@parkbenchsolutions.com>
2018-11-21 09:02:43 -0500

    call forward not reachable show on update

--- src/user/components/callForwardingNotReachable/userCallForwardingNotReachable.component.js
+++ src/user/components/callForwardingNotReachable/userCallForwardingNotReachable.component.js
@@ -54,7 +54,10 @@
     function update(settings, callback) {
       Alert.spinner.open()
       UserCallForwardingNotReachableService.update(ctrl.userId, settings)
-        .then(loadSettings)
+        // .then(loadSettings)
+        .then(function(data) {
+          ctrl.settings = data
+        })
         .then(function() {
           Alert.notify.success('Settings Updated')
           if (_.isFunction(callback)) callback()
