0e29607d6bf50bb7455104bc41e419b7ab4af5c3
dusty <ddoris@parkbenchsolutions.com>
2018-12-27 20:12:52 -0700

    change io connect signature

--- src/common/services/socket.service.js
+++ src/common/services/socket.service.js
@@ -4,7 +4,7 @@
 
   function Service($rootScope, APP) {
     return function(url) {
-      const socket = io.connect(url || APP.socketURL)
+      const socket = io(url || APP.socketURL)
 
       function on(eventName, callback) {
         socket.on(eventName, function() {
