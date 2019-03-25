5ee2cd98827ce0c1b3a7ed12bf4db228798c975a
dusty <ddoris@parkbenchsolutions.com>
2019-03-25 12:30:09 -0700

    new services

---
+++ src/common/components/select-phone-number/index.js
@@ -0,0 +1,90 @@
+import angular from 'angular'
+import _ from 'lodash'
+import template from './index.html'
+
+angular.module('odin.common').component('selectPhoneNumber', {
+  template,
+  controller,
+  bindings: {
+    serviceProviderId: '<',
+    groupId: '<',
+    phoneNumber: '<',
+    showAll: '<',
+    allowManual: '<',
+    onUpdate: '&'
+  }
+})
+
+controller.$inject = [
+  'Alert',
+  'NumberService',
+  'GroupNumberService',
+  'EventEmitter',
+  'HashService'
+]
+function controller(
+  Alert,
+  NumberService,
+  GroupNumberService,
+  EventEmitter,
+  HashService
+) {
+  var ctrl = this
+  ctrl.numbers = []
+  ctrl.$onInit = onInit
+  ctrl.edit = edit
+  ctrl.select = select
+  ctrl.updated = updated
+
+  function onInit() {
+    ctrl.modalId = HashService.guid()
+  }
+
+  function updated() {
+    sendUpdate(ctrl.phoneNumber)
+  }
+
+  function edit() {
+    Alert.spinner.open()
+    loadNumbers()
+      .then(function() {
+        ctrl.search = ''
+        Alert.modal.open(ctrl.modalId)
+      })
+      .catch(function(error) {
+        Alert.notify.danger(error)
+      })
+      .finally(function() {
+        Alert.spinner.close()
+      })
+  }
+
+  function select(phoneNumber) {
+    if (phoneNumber) {
+      phoneNumber.isSelected = true
+    }
+    Alert.modal.close(ctrl.modalId)
+    sendUpdate(phoneNumber)
+  }
+
+  function sendUpdate(phoneNumber) {
+    ctrl.onUpdate(EventEmitter({ phoneNumber: phoneNumber }))
+  }
+
+  function loadNumbers() {
+    return GroupNumberService.index(
+      ctrl.serviceProviderId,
+      ctrl.groupId,
+      ctrl.showAll ? 'summary' : 'available'
+    ).then(function(data) {
+      ctrl.numbers = NumberService.expand(data)
+      if (ctrl.phoneNumber) {
+        var myNumber = _.find(ctrl.numbers, { min: ctrl.phoneNumber })
+        if (!myNumber) {
+          ctrl.numbers.push({ min: ctrl.phoneNumber })
+        }
+      }
+      return ctrl.numbers
+    })
+  }
+}
