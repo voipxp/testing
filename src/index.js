916f86dcb93a895ae598d17593d3512dd0e12824
dusty <ddoris@parkbenchsolutions.com>
2019-04-14 10:36:30 -0700

    add helper app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
+import 'why-did-you-update-redux'
 
 import App from './components/app'
 import store from './store'
