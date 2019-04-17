1a1f3203f869328773de1f2e4021194ce6a4aed0
dusty <ddoris@parkbenchsolutions.com>
2019-04-16 17:30:13 -0700

    update

--- src/components/ui/modal.js
+++ src/components/ui/modal.js
@@ -1,5 +1,5 @@
 // TODO: form validity check
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { isFunction } from 'lodash'
