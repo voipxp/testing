62e0a3defb67349b5f3aa8351e7a7f8d5982dbf0
dusty <dusty@doris.name>
2019-05-20 06:36:44 -0700

    add some recipes from 'react-redux' for potential use

--- src/store/hooks.js
+++ src/store/hooks.js
@@ -1,7 +1,25 @@
-import { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
+import { useCallback, useMemo, bindActionCreators } from 'react'
+import { useDispatch, shallowEqual, useSelector } from 'react-redux'
 
 export const useAction = action => {
   const dispatch = useDispatch()
   return useCallback((...args) => dispatch(action(...args)), [action, dispatch])
+}
+
+/*
+  TODO: investigate these
+  https://react-redux.js.org/next/api/hooks#hooks-recipes
+*/
+export function useActions(actions, deps) {
+  const dispatch = useDispatch()
+  return useMemo(() => {
+    if (Array.isArray(actions)) {
+      return actions.map(a => bindActionCreators(a, dispatch))
+    }
+    return bindActionCreators(actions, dispatch)
+  }, [actions, dispatch])
+}
+
+export function useShallowEqualSelector(selector) {
+  return useSelector(selector, shallowEqual)
 }
