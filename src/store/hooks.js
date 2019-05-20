744fe041a5c2a82956087ba1cef28fa6092dbeb8
dusty <dusty@doris.name>
2019-05-20 08:46:37 -0700

    move useAction to utilize bindActionCreators

--- src/store/hooks.js
+++ src/store/hooks.js
@@ -1,24 +1,10 @@
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 import { useDispatch, shallowEqual, useSelector } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 export const useAction = action => {
   const dispatch = useDispatch()
-  return useCallback((...args) => dispatch(action(...args)), [action, dispatch])
-}
-
-/*
-  TODO: investigate these
-  https://react-redux.js.org/next/api/hooks#hooks-recipes
-*/
-export function useActions(actions, deps) {
-  const dispatch = useDispatch()
-  return useMemo(() => {
-    if (Array.isArray(actions)) {
-      return actions.map(a => bindActionCreators(a, dispatch))
-    }
-    return bindActionCreators(actions, dispatch)
-  }, [actions, dispatch])
+  return useMemo(() => bindActionCreators(action, dispatch), [action, dispatch])
 }
 
 export function useShallowEqualSelector(selector) {
