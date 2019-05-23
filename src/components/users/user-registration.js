9f4610e7bd7985ac4cea7b73ad5748859cf82b6a
Johnny Cash <developer@parkbenchsolutions.com>
2019-05-23 13:11:41 -0400

    added user registartion flyout

--- src/components/users/user-registration.js
+++ src/components/users/user-registration.js
@@ -1,13 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useUserRegistration } from '@/store/user-registration'
 import { useAlerts } from '@/store/alerts'
-import { UiCard, UiLoadingCard, UiDataTable } from '@/components/ui'
+import {
+  UiCard,
+  UiLoadingCard,
+  UiDataTable,
+  UiCardModal,
+  UiSection,
+  UiListItem,
+  UiButton
+} from '@/components/ui'
+import { Field, Column } from 'rbx'
 
 export const UserRegistration = ({ match }) => {
   const { userId } = match.params
   const { userRegistration, loadUserRegistration } = useUserRegistration(userId)
   const { alertDanger } = useAlerts()
+  const [showModal, setShowModal] = useState(false)
+  const [form, setForm] = useState({})
 
   const columns = [
     { key: 'deviceLevel', label: 'Level' },
@@ -22,6 +33,10 @@ export const UserRegistration = ({ match }) => {
     loadUserRegistration(userId).catch(alertDanger)
   }, [alertDanger, loadUserRegistration, userId])
 
+  function show(row) {
+    setForm({ ...row })
+    setShowModal(true)
+  }
   if (!userRegistration) return <UiLoadingCard />
 
   return (
@@ -32,8 +47,39 @@ export const UserRegistration = ({ match }) => {
           rows={userRegistration.registrations}
           rowKey="deviceName"
           hideSearch={true}
+          onClick={show}
         />
       </UiCard>
+      <UiCardModal
+        title="User Registration"
+        isOpen={showModal}
+        onCancel={() => setShowModal(false)}
+      >
+        <UiSection>
+          <UiListItem label="Device Level">{form.deviceLevel}</UiListItem>
+          <UiListItem label="Device Name">{form.deviceName}</UiListItem>
+          <UiListItem label="Order">{form.order}</UiListItem>
+          <UiListItem label="URI ">{form.uRI}</UiListItem>
+          <UiListItem label="Expiration ">{form.expiration}</UiListItem>
+          <UiListItem label="Line/Port ">{form.linePort}</UiListItem>
+          <UiListItem label="Endpoint Type">{form.endpointType}</UiListItem>
+          <UiListItem label="publicNetAddress">
+            {form.publicNetAddress}
+          </UiListItem>
+          <UiListItem label="publicPort">{form.publicPort}</UiListItem>
+          <UiListItem label="privateNetAddress">
+            {form.privateNetAddress}
+          </UiListItem>
+          <UiListItem label="privatePort">{form.privatePort}</UiListItem>
+          <UiListItem label="userAgent">{form.userAgent}</UiListItem>
+          <UiListItem label="lockoutStarted" disabled={!form.lockoutStarted}>
+            {form.lockoutStarted}
+          </UiListItem>
+          <UiListItem label="lockoutExpires">{form.lockoutExpires}</UiListItem>
+          <UiListItem label="lockoutCount">{form.lockoutCount}</UiListItem>
+          <UiListItem label="accessInfo">{form.accessInfo}</UiListItem>
+        </UiSection>
+      </UiCardModal>
     </>
   )
 }
