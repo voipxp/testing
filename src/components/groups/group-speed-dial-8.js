72edaae95e6ddd1f84c69ec5d08bce625d8a347c
dusty <dusty@doris.name>
2019-05-28 16:38:49 -0700

    fix table key

--- src/components/groups/group-speed-dial-8.js
+++ src/components/groups/group-speed-dial-8.js
@@ -29,14 +29,14 @@ export const GroupSpeedDial8 = ({ match }) => {
 
   const columns = [
     { key: 'userId', label: 'User ID' },
-    { key: 2, label: '2' },
-    { key: 3, label: '3' },
-    { key: 4, label: '4' },
-    { key: 5, label: '5' },
-    { key: 6, label: '6' },
-    { key: 7, label: '7' },
-    { key: 8, label: '8' },
-    { key: 9, label: '9' }
+    { key: '2', label: '2' },
+    { key: '3', label: '3' },
+    { key: '4', label: '4' },
+    { key: '5', label: '5' },
+    { key: '6', label: '6' },
+    { key: '7', label: '7' },
+    { key: '8', label: '8' },
+    { key: '9', label: '9' }
   ]
 
   useEffect(() => {
