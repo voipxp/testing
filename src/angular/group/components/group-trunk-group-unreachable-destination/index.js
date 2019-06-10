47dec13df4f2fc9873948237e2d1dc6719e8147a
dusty <dusty@doris.name>
2019-06-10 12:10:57 -0700

    fix unreachabledestination reroute options

--- src/angular/group/components/group-trunk-group-unreachable-destination/index.js
+++ src/angular/group/components/group-trunk-group-unreachable-destination/index.js
@@ -14,6 +14,25 @@ function controller(Alert, GroupTrunkGroupService) {
   var ctrl = this
   ctrl.options = GroupTrunkGroupService.options
   ctrl.edit = edit
+  ctrl.$onInit = onInit
+
+  function onInit() {
+    ctrl.loading = true
+    loadAvailableTrunks()
+      .catch(Alert.error)
+      .finally(() => (ctrl.loading = false))
+  }
+
+  function loadAvailableTrunks() {
+    return GroupTrunkGroupService.index(
+      ctrl.parent.serviceProviderId,
+      ctrl.parent.groupId,
+      {
+        includeEnterpriseTrunkGroups: true,
+        onlyTrunkGroupsWithDevice: true
+      }
+    ).then(data => (ctrl.availableTrunks = data))
+  }
 
   function edit() {
     ctrl.editTrunk = angular.copy(ctrl.parent.trunk)
