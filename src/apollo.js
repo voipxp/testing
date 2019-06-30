2fd2d567ab74f25d7f373e0599617d9c66862bc8
dusty <dusty@doris.name>
2019-06-30 11:58:59 -0700

    move auth into a middleware

--- src/apollo.js
+++ src/apollo.js
@@ -1,24 +1,23 @@
 import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { setContext } from 'apollo-link-context'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink, concat } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const STORAGE_KEY = 'odin:token'
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:10000/graphql'
-})
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(STORAGE_KEY)
-  return {
-    headers: { ...headers, authorization: token ? `Bearer ${token}` : '' }
-  }
-})
+const httpLink = new HttpLink({ uri: 'http://localhost:10000/graphql' })
 
 const cache = new InMemoryCache()
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(STORAGE_KEY)
+  operation.setContext({
+    headers: { authorization: token ? `Bearer ${token}` : null }
+  })
+  return forward(operation)
+})
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: concat(authMiddleware, httpLink),
   cache
 })
