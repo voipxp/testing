41eded85a6564e9d123b111838ed7cc85d96efa6
dusty <dusty@doris.name>
2019-08-16 11:41:48 -0700

    docz

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,9 @@
 export default {
-  title: "Odin UI",
-  src: "./src",
-  files: "**/*.mdx",
-  plugins: ["gatsby-plugin-sass"],
+  title: 'Odin UI',
+  src: './src',
+  files: '**/*.mdx',
+  plugins: ['gatsby-plugin-sass'],
   filterComponents: files => {
-    return files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath));
+    return files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath))
   }
-};
+}
