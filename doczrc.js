6633263457332b9fa39dedc289637337c7846e65
dusty <dusty@doris.name>
2019-08-16 14:38:35 -0700

    docs

--- doczrc.js
+++ doczrc.js
@@ -2,7 +2,6 @@ export default {
   title: 'Odin UI',
   src: './src',
   files: '**/*.mdx',
-  plugins: ['gatsby-plugin-sass'],
   filterComponents: files => {
     return files.filter(filepath => /[w-]*.(js|jsx|ts|tsx)$/.test(filepath))
   }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,6 @@
 const path = require('path')
-
 exports.onCreateWebpackConfig = ({ actions }) => {
-  const srcPath = path.resolve(__dirname, '..', 'src')
   actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /.(css|scss|sass)$/,
-          use: ['css-loader', 'postcss-loader', 'sass-loader']
-        }
-      ]
-    },
-    resolve: {
-      alias: {
-        '@': srcPath
-      }
-    }
+    resolve: { alias: { '@': path.resolve(__dirname, '..', 'src') } }
   })
 }
