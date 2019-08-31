dc4fd99c2c90af22c6c41fd6249f06329ada7d4a
dusty <dusty@doris.name>
2019-08-31 07:02:09 -0700

    update deps

--- src/components/ui/use-form.js
+++ src/components/ui/use-form.js
@@ -1,7 +1,5 @@
+// ONLY WAY I CAN FIGURE OUT HOW TO GET THE DESCRIPTION LOADED IN THE STORY
 import React from 'react'
-import { UiFormField, UiFormError } from '.'
-import { useForm } from '@/utils'
-import { Button, Input } from 'rbx'
 
 /**
  * Form helper to handle onChange events and track the validity of the form. Pass in a formRef to enable validations, then utilize HTML5 validation attributes on your inputs.
@@ -12,48 +10,6 @@ import { Button, Input } from 'rbx'
  * const {form, setForm, onChange, isValid, errors} = useForm(initialState, formRef)
  * ```
  */
-export const UseFormExample = () => {
-  const formRef = React.useRef()
-  const initialState = { firstName: '', lastName: '', age: '' }
-  const { form, onChange, isValid, errors } = useForm(initialState, formRef)
-  return (
-    <form ref={formRef} onSubmit={e => e.preventDefault()}>
-      <UiFormField label="First Name" horizontal>
-        <Input
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          onChange={onChange}
-          value={form.firstName}
-          minLength={5}
-        />
-        <UiFormError message={errors.firstName} />
-      </UiFormField>
-      <UiFormField label="Last Name" horizontal>
-        <Input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          onChange={onChange}
-          value={form.lastName}
-          minLength={5}
-        />
-        <UiFormError message={errors.lastName} />
-      </UiFormField>
-      <UiFormField label="Age" horizontal>
-        <Input
-          type="number"
-          placeholder="Age"
-          name="age"
-          onChange={onChange}
-          value={form.age}
-          max={10}
-        />
-        <UiFormError message={errors.age} />
-      </UiFormField>
-      <Button color="link" type="submit" disabled={!isValid}>
-        Submit
-      </Button>
-    </form>
-  )
+export const UseForm = () => {
+  return <p>Hello</p>
 }
