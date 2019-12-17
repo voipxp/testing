ceff0ce773b49db623b503c3119b74cce87f37ab
Johnny Cash <developer@parkbenchsolutions.com>
2019-12-17 15:25:47 -0500

    added export settings

--- src/components/audits/audit.js
+++ src/components/audits/audit.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types'
 import { useAsync } from 'react-async-hook'
 import { useAlerts } from '@/store/alerts'
 import auditApi from '@/api/audits'
+import settingsApi from '@/api/settings'
 import { AppBreadcrumb } from '@/components/app'
-import { Breadcrumb } from 'rbx'
+import { Breadcrumb, Column, Input, Select } from 'rbx'
+import axios from 'axios'
 import {
   UiCard,
   UiButton,
@@ -12,7 +14,8 @@ import {
   UiLoadingModal,
   UiDataTable,
   UiListItem,
-  UiCardModal
+  UiCardModal,
+  UiFormField
 } from '@/components/ui'
 
 const columns = [
@@ -29,13 +32,24 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
   const id = match.params.id
   const { alertDanger } = useAlerts()
   const [showModal, setShowModal] = useState(false)
+  const [showExportModal, setShowExportModal] = useState(false)
   const [showLoading, setShowLoading] = useState(false)
   const [data, setData] = useState('')
   const [serviceType, setServiceType] = useState('')
+  const [form, setForm] = useState({})
+  const [endpoints, setEndpoints] = useState([])
+  const KEY = 'exports'
 
   const { result, error, loading } = useAsync(() => auditApi.json(id), [id])
 
   if (error) alertDanger(error)
+
+  function handleInput(event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    setForm({ ...form, [name]: value })
+  }
 
   const downloadFile = async () => {
     const fileName = 'backup-' + id
@@ -49,12 +63,60 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
     link.click()
     document.body.removeChild(link)
   }
+  async function exportAudit() {
+    try {
+      setForm({
+        endpoint: '',
+        bwksUserId: '',
+        bwksPassword: ''
+      })
+      setShowLoading(true)
+      const end = await settingsApi.show(KEY)
+      setEndpoints(end.endpoints)
+      console.log('endpoints', endpoints)
+      setShowExportModal(true)
+    } catch (error_) {
+      setShowLoading(false)
+      alertDanger(error_)
+    } finally {
+      setShowLoading(false)
+    }
+  }
+
+  async function uploadExport() {
+    if (!form.endpoint) {
+      console.log('endpoint required')
+    }
+    console.log('form', form)
+    console.log('form.endpoint', form.endpoint)
+    setForm({
+      endpoint: form.endpoint,
+      bwksUserId: form.bwksUserId,
+      bwksPassword: form.bwksPassword
+    })
+    try {
+      const api = await axios.create({ baseURL: form.endpoint })
+      const token = await api.post('/auth/token', {
+        username: form.bwksUserId,
+        password: form.bwksPassword,
+        encryption: 'plain'
+      })
+      console.log('token', token)
+      setShowLoading(true)
+      setShowModal(false)
+    } catch (error_) {
+      alertDanger(error_)
+    } finally {
+      setShowLoading(false)
+    }
+  }
 
   async function open(data) {
     try {
       setShowLoading(true)
       setShowModal(true)
       const result = await auditApi.show(data.id, {})
+
       setServiceType(result.serviceType)
       setData(JSON.stringify(result, null, 2))
     } catch (error_) {
@@ -100,12 +162,20 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
           <UiCard
             title="Jobs"
             buttons={
-              <UiButton
-                color="link"
-                icon="download"
-                size="small"
-                onClick={downloadFile}
-              />
+              <>
+                <UiButton
+                  color="link"
+                  icon="upload"
+                  size="small"
+                  onClick={exportAudit}
+                />
+                <UiButton
+                  color="link"
+                  icon="download"
+                  size="small"
+                  onClick={downloadFile}
+                />
+              </>
             }
           >
             <UiDataTable
@@ -123,6 +193,68 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
       ) : (
         <UiCardModal title={serviceType} onCancel={onCancel} isOpen={showModal}>
           <pre className="prettyprint">{data}</pre>
+        </UiCardModal>
+      )}
+      {showLoading ? (
+        <UiLoadingModal isOpen={showLoading} />
+      ) : (
+        <UiCardModal
+          title={`Export Audit to ${form.name}`}
+          onCancel={() => setShowExportModal(false)}
+          onSave={uploadExport}
+          isOpen={showExportModal}
+        >
+          <form>
+            <Column.Group>
+              <Column>
+                <UiFormField label="odin System">
+                  <Select.Container fullwidth>
+                    <Select
+                      value={form.endpoint}
+                      onChange={handleInput}
+                      name="endpoint"
+                      disabled={showLoading}
+                      autoFocus
+                    >
+                      <Select.Option value="Please select...">
+                        {'Please select...'}
+                      </Select.Option>
+                      {endpoints.map(searchType => (
+                        <Select.Option
+                          key={searchType.key}
+                          value={searchType.url}
+                        >
+                          {searchType.name}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Select.Container>
+                </UiFormField>
+              </Column>
+              <Column>
+                <UiFormField label="broadworks userId">
+                  <Input
+                    type="text"
+                    name="bwksUserId"
+                    value={form.bwksUserId}
+                    onChange={handleInput}
+                    placeholder="Broadworks User ID"
+                  />
+                </UiFormField>
+              </Column>
+              <Column>
+                <UiFormField label="broadworks password">
+                  <Input
+                    type="password"
+                    name="bwksPassword"
+                    value={form.bwksPassword}
+                    onChange={handleInput}
+                    placeholder="Broadworks Password"
+                  />
+                </UiFormField>
+              </Column>
+            </Column.Group>
+          </form>
         </UiCardModal>
       )}
     </>
