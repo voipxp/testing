930c30a97fa7c2c8d4c96603ae9003529b7c308b
Johnny Cash <developer@parkbenchsolutions.com>
2020-01-07 08:27:27 -0500

    fixed component name for imports

--- src/components/resellers/reseller-imports.js
+++ src/components/resellers/reseller-imports.js
@@ -2,14 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Imports } from '../imports/imports'
 
-export const ResellerAudits = ({ match, history }) => {
+export const ResellerImports = ({ match, history }) => {
   // const open = ({ id }) => history.push(`/audits/${id}`)
   return (
     <Imports history={history} match={match} isBreadcrumb={false}></Imports>
   )
 }
 
-ResellerAudits.propTypes = {
+ResellerImports.propTypes = {
   history: PropTypes.object,
   match: PropTypes.object.isRequired
 }
