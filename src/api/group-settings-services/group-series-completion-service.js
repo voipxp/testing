2f3834a1c55f556bdc3088c982b69d3871327c0e
anshuporwal01 <anshup@voipxp.com>
2020-03-02 11:56:55 +0530

    fixes #114

--- src/api/group-settings-services/group-series-completion-service.js
+++ src/api/group-settings-services/group-series-completion-service.js
@@ -6,25 +6,24 @@ export function show(serviceProviderId, groupId) {
 }
  
 
-export function update(params) {
+export function update(params) { 
   return api.put('groups/series-completion', params)
 }
 
 export function store(object) { 
   return api.post('groups/series-completion', object)
 }
-
-export function destroy(object) {
-  return api.put('groups/series-completion', object)
+ 
+export function destroy(serviceProviderId, groupId , name) {
+  return api.delete('groups/series-completion', { params: { serviceProviderId, groupId , name } })
 }
 
-
-export function groupName(serviceProviderId, groupId, name) {
+export function groupDetail(serviceProviderId, groupId, name) {
   return api.get('groups/series-completion', { params: { serviceProviderId, groupId, name } })
 }
 
-export function usersGroup(serviceProviderId, groupId){
+export function users(serviceProviderId, groupId){
 	return api.get('groups/series-completion/users', { params: { serviceProviderId, groupId } })
 }
 
-export default { show, update, store,destroy  ,groupName , usersGroup}
+export default { show, update, store, destroy, groupDetail, users}
--- src/components/groups/group-series-completion.js
+++ src/components/groups/group-series-completion.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import { useUi } from '@/store/ui'
-import { Input , Breadcrumb } from 'rbx'
+/* eslint-disable array-callback-return */
+import React, { useState, useEffect } from 'react'
+ 
+import { Input , Button, Breadcrumb } from 'rbx'
 import { AppBreadcrumb } from '@/components/app'
+import PropTypes from 'prop-types'
+//import apiCommProfile from '@/api/group-communication-barring-profiles'
+//import apiComm from '@/api/group-communication-barring'
+
+import apiSeriesCompletion from '@/api/group-settings-services/group-series-completion-service'
+import { orderBy } from 'natural-orderby'
+import { hideLoadingModal } from '@/store/ui'
+import { Select } from 'rbx'
 import { useAlerts } from '@/store/alerts'
-import { useQuery, setQueryData } from 'react-query'
-import api from '@/api/group-settings-services/group-series-completion-service'
 import {
-  UiButton,
-  UiCardModal,
   UiCard,
   UiLoadingCard,
   UiSection,
+  UiButton,
+  UiListItem,
+  UiCheckbox,
+  UiInputCheckbox,
+  UiCardModal,
   UiFormField,
-  UiDataTable ,
+  UiDataTable,
   UiSelectableTable
 } from '@/components/ui'
+ 
+
 export const GroupSeriesCompletion = ({ match }) => {
-  
   const initialForm = {
     isCreate: true,
     "serviceProviderId":'',
@@ -25,229 +35,261 @@ export const GroupSeriesCompletion = ({ match }) => {
     "name":'',
     "users":[]
   }
-
-   
-  
-  const { serviceProviderId,groupId} = match.params
-  //const { userId } = match.params
+  const { serviceProviderId, groupId } = match.params
   const { alertSuccess, alertDanger } = useAlerts()
-  const { showLoadingModal, hideLoadingModal } = useUi()
-  const [form, setForm] = useState(initialForm)
+  const [loading, setLoading] = useState(true)
+  const [groupSeriesCompletion, setGroupSeriesCompletion] = useState([]) 
   const [showModal, setShowModal] = useState(false)
+  
+  const [form, setForm] = useState({})
+  const [availableUser, setAvailableUser] = useState([])
+  const [selectedUser, setSelectedUser] = useState([])
+  const [allAvailableUser, setAllAvailableUser] = useState([])
+  const [addConfirm, setAddConfirm] = useState(false)
   const [showConfirm, setShowConfirm] = useState(false)
-  const [availableUser, setAvailableUser] = useState([
-        
-          {userId: "test@parkbenchsolutions.com"},
-          {userId: "gggg1111@parkbenchsolutions.com"}
-      
-  ])
-  const [selectedUser, setSelectedUser] = useState([
-    {userId: "tesy@parkbenchsolutions.com"},
-  {userId: "gggg@parkbenchsolutions.com"}
-  ])
-  const seriesCompletionNames = [] 
-
-  const { data: result, isLoading, error } =  useQuery(
-    'series-completion',
-    () => api.show(serviceProviderId,groupId)
-  )
+  const [canSelectedUser, setCanSelectedUser] = useState(true)
   
-  const userServiceData = result || {} 
-  const seriesCompletion  =  userServiceData.names || []
-   /*
+  const seriesCompletionNames = []
 
-  const { data: available, aLoading, aError } =  useQuery(
-    'series-completion-available',
-    () => api.usersGroup(serviceProviderId,groupId)
-  ) */
+  const loadSeriesCompletions = React.useCallback(async () => {
+    try {
+      const data = await apiSeriesCompletion.show(serviceProviderId, groupId)
+      setGroupSeriesCompletion(data)
+     // setgroupSeriesCompetionNames(data.names)
 
-
-  //const availableUsers =  available || {}
-  //const selectedUser =  availableUsers
-  
-
-  //setSelectedUser(selectedUsers) 
-  //const seriesCompletionNames = [];
-   // eslint-disable-next-line array-callback-return
-  seriesCompletion.map(function(el) {
-    seriesCompletionNames.push({
-      names: el
-    })
-  }) 
-
-
-  const columnsData = [
-    { key: 'names', label: 'Group Name' }
-  ]
-    
-  const remove = () => {
-      setShowConfirm(false)
-     // destroy(serviceProviderId, groupId)
+      const avaliableUsersList = await apiSeriesCompletion.users(serviceProviderId, groupId)
+      setAvailableUser(avaliableUsersList)
+      setAllAvailableUser(avaliableUsersList) 
+    } catch (error) {
+      alertDanger(error)
+    } finally {
+      setLoading(false)
     }
-
-  if (error) alertDanger(error)
-  if (isLoading) return <UiLoadingCard />
-
+  }, [serviceProviderId, groupId, alertDanger]) 
   
-  function handleInput(event) { 
+
+  useEffect(() => {
+    setLoading(true)
+    loadSeriesCompletions()  
+  }, [alertDanger, loadSeriesCompletions])
+
+
+  const seriesCompletionName  =  groupSeriesCompletion.names ||  [] 
+  const sortGroupCompletionName = React.useMemo(() => {
+  const sortedValues =   orderBy( seriesCompletionName, shortedValue => shortedValue )
+    sortedValues.map(function(el) {
+        seriesCompletionNames.push({
+          names: el
+        })
+    })
+    return seriesCompletionNames
+  }, [seriesCompletionName , seriesCompletionNames] )
+  
+  if( canSelectedUser === true ){
+    if(seriesCompletionNames.length > 0 ){
+      setCanSelectedUser(false)
+      getGroupDetails(serviceProviderId , groupId , sortGroupCompletionName[0].names)
+    }
+  }
+
+ async function getGroupDetails(serviceProviderId,groupId , name){ 
+  setForm({
+    "serviceProviderId":serviceProviderId,
+    "groupId":groupId,
+    "name":name,
+    "users":selectedUser
+  })
+  try {
+    const data1 = await apiSeriesCompletion.groupDetails(serviceProviderId, groupId , name)
+    const dataSelected = data1.users || [] 
+    setSelectedUser(dataSelected)
+  } catch (error) {
+    alertDanger(error)
+    setShowModal(true)
+  } finally {
+    setLoading(false)
+    hideLoadingModal()
+  }
+}
+
+console.log(availableUser)
+ 
+const columnsData = [
+  { key: 'names', label: 'Group Name' }
+]
+ 
+  
+  function handleInput(event) {
     const target = event.target
     const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name 
-	  setForm({ ...form, [name]: value })
-	}
-  
+    const name = target.name
+    setForm({ ...form, [name]: value })
+  }
+
+  async function onSelect(rows) {
+    setForm({
+      ...rows,
+      "serviceProviderId":serviceProviderId,
+      "groupId":groupId,
+      "name":rows.names,
+      "users":selectedUser
+    })
+    
+    getGroupDetails(serviceProviderId , groupId , rows.names)
+    setAvailableUser(allAvailableUser)
+  } 
+
   function add() {
+    setAddConfirm(true)
     setForm({ ...initialForm })
     setShowModal(true)
   }
-  
-  function open(selectedRow){
+
+  function edit() { 
+    setAddConfirm(false)
+    const editFormDetails  = {
+      "serviceProviderId":serviceProviderId,
+      "groupId":groupId,
+      "name":form.name,  
+      "users":selectedUser 
+    }
+    setForm({ ...editFormDetails })
+    update(editFormDetails)
+  }
+
+  async function create(form) {    
+    const postCreateData  = {
+      "serviceProviderId":serviceProviderId,
+      "groupId":groupId,
+      "name":form.names,  
+      "users":[] 
+    } 
+    setShowModal(true)
+    setCanSelectedUser(true)
+    try {
+      await apiSeriesCompletion.store(postCreateData)
+      await loadSeriesCompletions()
+      setCanSelectedUser(true)
+      alertSuccess('Series Completion Updated')
+    } catch (error) {
+      alertDanger(error)
+      setShowModal(true)
+    } finally {
+      setLoading(false)
+      setShowModal(false)
+    }
      
   }
-/*
-*create new user speed dial 100
-*/
 
-async function create(data) {   
-  const postCreateData  = {
-    "serviceProviderId":serviceProviderId,
-    "groupId":groupId,
-    "name":data.names,
-    "users":[]
-  }
-  showLoadingModal()
-  try {
-    const getresponseData =   await api.store(postCreateData)
-    setQueryData(['series-completion'], getresponseData, {
-    shouldRefetch: true,
-  })
-    alertSuccess('Group Series Competed Created')
-    setForm({ ...getresponseData })
-    setShowModal(false)
-  } catch (error_) {
-    alertDanger(error_)
-  } finally {
-    hideLoadingModal()
-  }
-} 
   function save() { 
-    form.isCreate ? create(form) : update(form)
+    create()
   }
-  
-  async function update(data) { 
-      
-    showLoadingModal()
+
+  const remove = () => {
+    setShowConfirm(false)
+    destroy(form)
+  }
+
+  async function destroy(form) { 
+    setLoading(true)
      
     try {
-      const newUserCallForwardingAlways = await api.update(data)
-      setQueryData(['series-completion'], newUserCallForwardingAlways, {
-        shouldRefetch: true,
-      })
-      alertSuccess('Alternate Numbers Setting Updated')
-      setShowModal(false)
-    } catch (error_) {
-      alertDanger(error_)
+      await apiSeriesCompletion.destroy(serviceProviderId, groupId , form.name)
+      await loadSeriesCompletions()
+      setCanSelectedUser(true)
+      alertSuccess('series-completion Deleted')
+    } catch (error) {
+      alertDanger(error)
     } finally {
+      setLoading(false)
+    }
+  }
+
+  async function update(profile) {
+    setShowModal(false)
+    setLoading(true)
+    try {
+      await apiSeriesCompletion.update(profile) 
+      await loadSeriesCompletions()
+      setCanSelectedUser(true)
+      alertSuccess('Series Completion Updated')
+    } catch (error) {
+      alertDanger(error)
+      setShowModal(true)
+    } finally {
+      setLoading(false)
       hideLoadingModal()
     }
   }
- 
+    
+  if(loading) return <UiLoadingCard />
   return (  
     <>
-    <AppBreadcrumb>
-      <Breadcrumb.Item>Series Completion</Breadcrumb.Item>
-    </AppBreadcrumb>
-
-    <UiCard
-      title="Series Completion"
-      buttons={
-        <UiButton color="link" icon="add" size="small" onClick={add} />
-      }
-    >
-       <UiSelectableTable
-         title="Users"
-         availableUser={availableUser}
-         setAvailableUser={(availableItem) => setAvailableUser(availableItem)}
-         selectedUser={selectedUser}
-         setSelectedUser={(selectedItem) => setSelectedUser(selectedItem)}
-         rowKey='userId'
-       />
-         {form.isCreate ?
-        
-       <UiDataTable
-       columns={columnsData}
-       rows={seriesCompletionNames}
-       rowKey="names"
-       hideSearch={false}
-       onClick={open}
-       pageSize={20}
-     />
-        : ''
-      }
+      <AppBreadcrumb>
+        <Breadcrumb.Item>series Completion</Breadcrumb.Item>
+      </AppBreadcrumb>
+      <UiCard
+        title="series Completion"
+        buttons={
+          <UiButton color="link" icon="add" size="small" onClick={add} />
+        }
+      >
+      <UiDataTable
+          columns={columnsData}
+          rows={seriesCompletionNames}
+          rowKey="names"
+          hideSearch={false}
+          onClick={onSelect}
+          showSelect={true}
+          pageSize={5}
+        />
       </UiCard>
       <UiCardModal
-          title={form.isCreate ? 'Group Name' :(`Edit Group Name : ${form.names}`) }
-          isOpen={showModal}
-          onCancel={() => setShowModal(false)}
-          onSave={save}
-          onDelete={form.isCreate ? null : () => setShowConfirm(true)}
-        >
+        title= 'Group Name'
+        isOpen={showModal}
+        onCancel={() => setShowModal(false)}
+        onSave={save}
+        onDelete={addConfirm ? null : () => setShowConfirm(true)}
+      >
         <form>
           <UiSection>
-          { (form.isCreate ? (
-              <>
-                 <UiFormField label="Group Name" horizontal>
-                  <Input
-                  type="text"
-                  name="names"
-                  value={form.names}
-                  onChange={handleInput}
-                  />
-                </UiFormField>
-                
-
-              </>
-            ) : (
-                  <>
-                    <UiFormField label="Phone Number" horizontal>
-                      <Input
-                        type="text"
-                        name="phoneNumber"
-                        value=""
-                        onChange={handleInput}
-                      />
-                    </UiFormField>
-                    <UiFormField label="Description" horizontal>
-                      <Input
-                        type="text"
-                        name="description"
-                        value=""
-                        onChange={handleInput}
-                      />
-                    </UiFormField>
-                  </>
-                )
-              )
-            }
+            <UiFormField label="Group Name" horizontal>
+              <Input
+              type="text"
+              name="names"
+              value={form.names}
+              onChange={handleInput}
+              />
+            </UiFormField>
           </UiSection>
         </form>
       </UiCardModal>
+      <UiCard title={(`Series Completion Group Name : ${form.name}`) } >
+        <UiSelectableTable
+          title="Users"
+          availableUser={availableUser}
+          setAvailableUser={(availableItem) => setAvailableUser(availableItem)}
+          selectedUser={selectedUser}
+          setSelectedUser={(selectedItem) => setSelectedUser(selectedItem)}
+          rowKey='userId'
+        />
+        <Button.Group align="right" style={{ margin: '1rem 0rem' }}  > 
+          <Button color='danger' onClick={() => setShowConfirm(true)} >Delete</Button>
+          <Button color='success' onClick={edit}>Edit</Button>
+        </Button.Group>
+      </UiCard>
       <UiCardModal
-            title="Please Confirm"
-            isOpen={showConfirm}
-            onCancel={() => setShowConfirm(false)}
-            onDelete={remove}
-          >
-            <blockquote>
-              Are you sure you want to Remove this Series Completion Id?
-            </blockquote>
-          </UiCardModal>
+        title="Please Confirm"
+        isOpen={showConfirm}
+        onCancel={() => setShowConfirm(false)}
+        onDelete={remove}
+      >
+        <blockquote>
+          Are you sure you want to Remove this Series Completion Group {form.name} ?
+        </blockquote>
+      </UiCardModal>
     </>
-  
   )
- 
- 
-
 }
 GroupSeriesCompletion.propTypes = {
   match: PropTypes.object.isRequired
