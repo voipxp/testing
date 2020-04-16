fa40867b7a4d967038231a725be0b5bcbac3a79b
anshuporwal01 <anshup@voipxp.com>
2020-04-16 11:06:45 +0530

    changes

--- src/components/resellers/reseller-audits.js
+++ src/components/resellers/reseller-audits.js
@@ -2,12 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Audits } from '../audits/audits'
 
-export const ServiceProviderAudits = ({ match, history }) => {
+export const ResellerAudits = ({ match, history }) => {
   // const open = ({ id }) => history.push(`/audits/${id}`)
   return <Audits history={history} match={match} isBreadcrumb={false}></Audits>
 }
 
-ServiceProviderAudits.propTypes = {
+ResellerAudits.propTypes = {
   history: PropTypes.object,
   match: PropTypes.object.isRequired
 }
