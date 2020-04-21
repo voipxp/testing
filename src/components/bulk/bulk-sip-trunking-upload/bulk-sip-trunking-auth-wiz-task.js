3ac39fc578f4ac3db6661e74ea9afc284e5f944f
pankajs1306 <57499287+pankajs1306@users.noreply.github.com>
2020-04-21 17:02:14 +0530

    SIP Trunking

--- src/components/bulk/bulk-sip-trunking-upload/bulk-sip-trunking-auth-wiz-task.js
+++ src/components/bulk/bulk-sip-trunking-upload/bulk-sip-trunking-auth-wiz-task.js
@@ -6,14 +6,22 @@ import { BulkImportUpload } from '@/components/bulk/bulk-sip-trunking-upload/bul
 export const BulkSipTrunkingAuthWizTask = props => {
   const [isNextBtnDisabled, setDisableNextButton] = React.useState(false)
 
+  const onComplete = (obj) => {
+    const isCompleted = obj.isCompleted
+    props.whenTaskIsCompleted(props.name, isCompleted)
+    //if(isCompleted) props.complete()
+  }
+
   const memoizedValue = useMemo(
     () => (
       <BulkImportUpload
         {...props}
         expectedTaskType={props.task}
         setDisableNextButton={boolValue => setDisableNextButton(boolValue)}
+        onComplete={obj => onComplete(obj)}
       />
     ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [props]
   )
 
@@ -37,5 +45,8 @@ export const BulkSipTrunkingAuthWizTask = props => {
 BulkSipTrunkingAuthWizTask.propTypes = {
   localStorageKey: PropTypes.string,
   task: PropTypes.string,
-  complete: PropTypes.func
+  name: PropTypes.string,
+  complete: PropTypes.func,
+  whenTaskIsCompleted: PropTypes.func
+
 }
