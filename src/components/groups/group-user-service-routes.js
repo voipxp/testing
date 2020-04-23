37879254582445f047eeab3fe13a2b1ebd0e2755
anshuporwal01 <anshup@voipxp.com>
2020-04-23 12:55:01 +0530

    user services

--- src/components/groups/group-user-service-routes.js
+++ src/components/groups/group-user-service-routes.js
@@ -1,142 +1,96 @@
 import {
-  GroupSeriesCompletion
-} from '././group-series-completion'
-import {
-  GroupAutoAttendants,
-  GroupCallCenters,
-  GroupCallPark,
-  GroupCallPickups,
-  GroupCollaborate,
-  GroupFlexibleSeatingHosts,
-  GroupHuntGroups,
-  GroupMeetMe,
-  GroupMusicOnHold,
-  GroupNightForwarding,
-  GroupPagingGroups,
-  GroupTrunkGroups,
-  GroupVoiceMessaging,
-  GroupVirtualOnNetEnterpriseExtensions
-} from '@/components/groups/service-settings'
-  
+  GroupSpeedDial8
+} from './group-speed-dial-8'
 export const groupUserServiceRoutes = [
-        {
-          name :'Auto Receptionist',
-          path: 'auto-attendants',
-          component: GroupAutoAttendants,
-          hasModuleRead: 'Auto Attendant',
-          hasGroupService: ['Auto Attendant'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Call Center', 
-          path: 'call-centers', 
-          component: GroupCallCenters,
-          hasModuleRead: 'Call Center',
-          hasGroupService: ['Call Center - Basic', '	Call Center Monitoring', 'Call Center - Premium', 'Call Center - Standard']
-        },
-        {
-          name :'Call Park',
-          path: 'call-park',
-          component: GroupCallPark,
-          hasModuleRead: 'Call Park',
-          hasGroupService: ['Call Park'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Call Pickup',
-          path: 'call-pickup',
-          component: GroupCallPickups,
-          hasModuleRead: 'Call Pickup',
-          hasGroupService: ['Call Pickup'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Collaborate',
-          path: 'collaborate',
-          component: GroupCollaborate,
-          hasModuleRead: 'Collaborate - Audio',
-          hasGroupService: ['Collaborate - Audio'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Flexible Seating Host',
-          path: 'flexible-seating-hosts',
-          component: GroupFlexibleSeatingHosts,
-          hasModuleRead: 'Flexible Seating Host',
-          hasGroupService: ['Flexible Seating Guest'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Hunt Group',
-          component: GroupHuntGroups,
-          path: 'hunt-groups',
-          hasModuleRead: 'Hunt Group',
-          hasGroupService: ['Hunt Group'],
-          hasLevel: 'Group',
-        },
-        {
-          name :'Meet-Me Conferencing',
-          path: 'meet-me-conferencing',
-          component: GroupMeetMe,
-          hasModuleRead: 'Meet-Me Conferencing',
-          hasGroupService: ['Meet-Me Conferencing'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Music On Hold',
-          path: 'music-on-hold',
-          component: GroupMusicOnHold,
-          hasModuleRead: 'Music On Hold',
-          hasGroupService: ['Music On Hold'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Group Night Forwarding',
-          path: 'group-night-forwarding',
-          component: GroupNightForwarding,
-          hasModuleRead: 'Group Night Forwarding',
-          hasGroupService: ['Group Night Forwarding'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Group Paging',
-          path: 'group-paging',
-          component: GroupPagingGroups,
-          hasModuleRead: 'Group Paging',
-          hasGroupService: ['Group Paging'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Series Completion',
-          path: 'series-completion',
-          component: GroupSeriesCompletion,
-          hasModuleRead: 'Series Completion',
-          hasGroupService: ['Series Completion'],
-          hasLevel: 'Group',
-		  isBreadcrumb:false
-        },
-        {
-          name :'Trunk Group',
-          path: 'trunk-groups',
-          component: GroupTrunkGroups,
-          hasModuleRead: 'Trunk Group',
-          hasGroupService: ['Trunk Group'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Virtual On-Net Enterprise Extensions',
-          path: 'virtual-on-net-enterprise-extensions',
-          component: GroupVirtualOnNetEnterpriseExtensions,
-          hasModuleRead: 'Virtual On-Net Enterprise Extensions',
-          hasGroupService: ['Virtual On-Net Enterprise Extensions'],
-          hasLevel: 'Group'
-        },
-        {
-          name :'Voice Messaging Group',
-          component: GroupVoiceMessaging,
-          hasModuleRead: 'Voice Messaging Group',
-          path: 'voice-messaging-group',
-          hasGroupService: ['Voice Messaging Group'],
-          hasLevel: 'Group'
-        }
+  {
+    name: 'Call Forwarding Always',
+    path: 'call-forwarding-always',
+    angularComponent: 'groupCallForwardingAlways',
+    hasModuleRead: 'Call Forwarding Always',
+    hasGroupService: ['Call Forwarding Always'],
+    hasLevel: 'Group'
+  }, 
+  {
+    name: 'Call Forwarding Busy',
+	path: 'call-forwarding-busy',
+    angularComponent: 'groupCallForwardingBusy',
+    hasModuleRead: 'Call Forwarding Busy',
+    hasGroupService: ['Call Forwarding Busy'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Call Forwarding No Answer',
+    path: 'call-forwarding-no-answer',
+    angularComponent: 'groupCallForwardingNoAnswer',
+    hasModuleRead: 'Call Forwarding No Answer',
+    hasGroupService: ['Call Forwarding No Answer'],
+    hasLevel: 'Group'
+  },
+  {
+    name : 'Call Forwarding Not Reachable',
+    path: 'call-forwarding-not-reachable',
+    angularComponent: 'groupCallForwardingNotReachable',
+    hasModuleRead: 'Call Forwarding Not Reachable',
+    hasGroupService: ['Call Forwarding Not Reachable'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Calling Line ID Delivery Blocking',
+    angularComponent: 'groupCallingLineIdDeliveryBlocking',
+    path: 'calling-line-id-delivery-blocking',
+    hasModuleRead: 'Calling Line ID Delivery Blocking',
+    hasGroupService: ['Calling Line ID Delivery Blocking'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Call Recording',
+    angularComponent: 'groupCallRecording',
+    path: 'call-recording',
+    hasModuleRead: 'Call Recording',
+    hasGroupService: ['Call Recording'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Hoteling Guest',
+    angularComponent: 'groupHotelingGuest',
+    path: 'hoteling-guest',
+    hasModuleRead: 'Hoteling Guest',
+    hasGroupService: ['Hoteling Guest'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Hoteling Host',
+    path: 'hoteling-host',
+    angularComponent: 'groupHotelingHost',
+    hasModuleRead: 'Hoteling Host',
+    hasGroupService: ['Hoteling Host'],
+    hasLevel: 'Group'
+  },
+  {
+    name: 'Outgoing Calling Plan',
+    path: 'outgoing-users',
+    angularComponent: 'groupOutgoingCallingPlanUsers',
+    hasModuleRead: 'Outgoing Calling Plan',
+    hasGroupService: ['Outgoing Calling Plan'],
+    hasLevel: 'Group',
+    hasModuleReadRead: 'Group Calling Plan'
+  },
+  {
+    name: 'Speed Dial 8',
+    path: 'speedDial8',
+    component: GroupSpeedDial8,
+    hasModuleRead: 'Speed Dial 8',
+    hasGroupService: ['Speed Dial 8'],
+    hasLevel: 'Group',
+	  isBreadcrumb:false
+  },
+  {
+    name: 'Voice Messaging User',
+    angularComponent: 'groupVoiceMessagingUser',
+    path: 'voice-messaging-user',
+    hasModuleRead: 'Voice Messaging User',
+    hasGroupService: ['Voice Messaging User'],
+    hasLevel: 'Group'
+  }
 ]
+ 
