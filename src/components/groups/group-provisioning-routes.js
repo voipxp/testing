95d820bd80cd58c6ed98bea838cec2ec3b235f40
anshuporwal01 <anshup@voipxp.com>
2020-04-23 18:29:39 +0530

    changes

--- src/components/groups/group-provisioning-routes.js
+++ src/components/groups/group-provisioning-routes.js
@@ -4,64 +4,63 @@ import {
 export const ProvisioningRoutes = [
      {
         name: 'Assign Numbers',
-		path: 'groupNumbers',
+		    path: 'groupNumbers',
         angularComponent: 'groupNumbers',
         hasLevel: 'Service Provider',
         isPaasAdmin: true
       },
       {
         name: 'Devices',
-		path: 'groupDevices',
-		angularComponent: 'groupDevices',
+        path: 'groupDevices',
+        angularComponent: 'groupDevices',
         hasLevel: 'Group',
         isPaasAdmin: true
       },
       {
         
-	    name: 'Device Configuration',
-		path: 'groupDeviceTypes',
+	      name: 'Device Configuration',
+		    path: 'groupDeviceTypes',
         angularComponent: 'groupDeviceTypes',
         hasLevel: 'Group',
         isPaasAdmin: true
       },
 	  {
-        
-	    name: 'Group Extension Length',
-		path: 'groupExtensionLength',
+        name: 'Group Extension Length',
+		    path: 'groupExtensionLength',
         component: GroupExtensionLength,
         hasLevel: 'Group',
         isPaasAdmin: true,
-		isBreadcrumb: false
+		    isBreadcrumb: false
       },
       {
         name: 'User Services',
-		path: 'userServices',
+		    path: 'userServices',
         angularComponent: 'groupServices',
-		hasLevel: 'Service Provider',
-		serviceType: 'userServices',
+		    hasLevel: 'Service Provider',
+		    serviceType: 'userServices',
         isPaasAdmin: true
       },
       {
         name: 'Group Services',
-		path: 'groupServices',
-		angularComponent: 'groupServices',
-		hasLevel: 'Service Provider',
-		serviceType: 'groupServices',
+        path: 'groupServices',
+        angularComponent: 'groupServices',
+        hasLevel: 'Service Provider',
+        serviceType: 'groupServices',
         isPaasAdmin: true
       },
       {
         name: 'Service Packs',
-		path: 'servicePackServices',
-		angularComponent: 'groupServices',
-		serviceType: 'servicePackServices',
-		hasLevel: 'Service Provider',
+        path: 'servicePacks',
+        angularComponent: 'groupServices',
+        serviceType: 'servicePackServices',
+        hasLevel: 'Service Provider',
         isPaasAdmin: true
       },
       {
         name: 'Delete Group',
         path:  'groupDelete' ,
         angularComponent: 'groupDelete',
-		hasLevel: 'Service Provider',
+		    hasLevel: 'Service Provider',
         serviceProviderPolicy: 'groupDelete',
         isPaasAdmin: true
       },
