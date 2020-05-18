77291b25b163d10f8708a99bf7245614ec8c7534
Johnny Cash <developer@parkbenchsolutions.com>
2020-05-18 19:53:20 -0400

    changed layout of the group menu

--- src/components/groups/group-dashboard-menu.js
+++ src/components/groups/group-dashboard-menu.js
@@ -1,12 +1,10 @@
 import { GroupUserServiceSettings } from './group-user-service-settings'
 import { GroupServiceSettings } from './group-service-settings'
 import { GroupDepartments } from './group-departments'
-import {GroupPhoneListWithDirecotorySettings} from './group-phone-list-with-directory-settings'
-import {GroupCommunicationSettings} from './group-communication-settings'
-import {GroupWebPolicy} from './group-web-policy'
-import {
-  GroupExtensionLength
-} from './group-extension-length'
+import { GroupPhoneListWithDirecotorySettings } from './group-phone-list-with-directory-settings'
+import { GroupCommunicationSettings } from './group-communication-settings'
+import { GroupWebPolicy } from './group-web-policy'
+import { GroupExtensionLength } from './group-extension-length'
 import { groupReportRoutes } from './group-report-routes'
 
 export const dashboardMenu = [
@@ -24,43 +22,51 @@ export const dashboardMenu = [
         name: 'Business Profile',
         path: 'profile',
         angularComponent: 'groupProfile',
-        policy: 'profileRead',
+        policy: 'profileRead'
       },
       {
-          name: 'Departments',
-          path: 'departments',
-          component: GroupDepartments,
-          policy: 'departmentRead',
-          hasLevel: 'Group'
-        },
-        {
-          name: 'Users',
-          path: 'users',
-          angularComponent: 'groupUsers',
-          default: true
-        }
-      ]
+        name: 'Departments',
+        path: 'departments',
+        component: GroupDepartments,
+        policy: 'departmentRead',
+        hasLevel: 'Group'
+      },
+      {
+        name: 'Users',
+        path: 'users',
+        angularComponent: 'groupUsers',
+        default: true
+      },
+      {
+        name: 'Group Settings',
+        path: 'group-services',
+        component: GroupServiceSettings,
+        serviceType: 'groupServices'
+      },
+      {
+        path: 'user-services',
+        name: 'User Settings',
+        component: GroupUserServiceSettings
+      }
+    ]
   },
   {
-    label: 'Services',
+    label: 'REPORTS',
     items: [
-        {
-          name: 'Group Services',
-          path: 'group-services',
-          component: GroupServiceSettings,
-          serviceType: 'groupServices'
-        },
-        {
-          path: 'user-services',
-          name: 'User Services',
-          component: GroupUserServiceSettings
-        }
-      ]
+      {
+        name: 'Call Reports',
+        subMenus: groupReportRoutes.callReports
+      },
+      {
+        name: 'Utilization Reports',
+        subMenus: groupReportRoutes.utilizationReports
+      }
+    ]
   },
   {
     label: 'PROVISIONING',
     items: [
-	{
+      {
         name: 'Bulk Provisioning',
         path: 'bulk-provisioning',
         subMenus: [
@@ -91,158 +97,145 @@ export const dashboardMenu = [
         name: 'Resources',
         subMenus: [
           {
-               name: 'Assign Numbers',
-               path: 'groupNumbers',
-               angularComponent: 'groupNumbers',
-               hasLevel: 'Service Provider',
-               isPaasAdmin: true
-             },
-       {
-
-               name: 'Device Configuration',
-               path: 'groupDeviceTypes',
-               angularComponent: 'groupDeviceTypes',
-               hasLevel: 'Group',
-               isPaasAdmin: true
-             },
-                    {
-               name: 'Devices',
-               path: 'groupDevices',
-               angularComponent: 'groupDevices',
-               hasLevel: 'Group',
-               isPaasAdmin: true
-             },
-       {
-               name: 'Group Extension Length',
-               path: 'groupExtensionLength',
-               component: GroupExtensionLength,
-               hasLevel: 'Group',
-               isPaasAdmin: true,
-               isBreadcrumb: false
-             },
-       {
-               name: 'Group Services',
-               path: 'groupServices',
-               angularComponent: 'groupServices',
-               hasLevel: 'Service Provider',
-               serviceType: 'groupServices',
-               isPaasAdmin: true
-             },
-       {
-               name: 'Service Packs',
-               path: 'servicePacks',
-               angularComponent: 'groupServices',
-               serviceType: 'servicePackServices',
-               hasLevel: 'Service Provider',
-               isPaasAdmin: true
-             },
-       {
-               name: 'User Services',
-               path: 'userServices',
-               angularComponent: 'groupServices',
-               hasLevel: 'Service Provider',
-               serviceType: 'userServices',
-               isPaasAdmin: true
-             },
-       {
-               name: 'Delete Group',
-               path:  'groupDelete' ,
-               angularComponent: 'groupDelete',
-               hasLevel: 'Service Provider',
-               serviceProviderPolicy: 'groupDelete',
-               isPaasAdmin: true
-             }
+            name: 'Assign Numbers',
+            path: 'groupNumbers',
+            angularComponent: 'groupNumbers',
+            hasLevel: 'Service Provider',
+            isPaasAdmin: true
+          },
+          {
+            name: 'Device Configuration',
+            path: 'groupDeviceTypes',
+            angularComponent: 'groupDeviceTypes',
+            hasLevel: 'Group',
+            isPaasAdmin: true
+          },
+          {
+            name: 'Devices',
+            path: 'groupDevices',
+            angularComponent: 'groupDevices',
+            hasLevel: 'Group',
+            isPaasAdmin: true
+          },
+          {
+            name: 'Group Extension Length',
+            path: 'groupExtensionLength',
+            component: GroupExtensionLength,
+            hasLevel: 'Group',
+            isPaasAdmin: true,
+            isBreadcrumb: false
+          },
+          {
+            name: 'Group Licenses',
+            path: 'groupServices',
+            angularComponent: 'groupServices',
+            hasLevel: 'Service Provider',
+            serviceType: 'groupServices',
+            isPaasAdmin: true
+          },
+          {
+            name: 'Service Pack Licenses',
+            path: 'servicePacks',
+            angularComponent: 'groupServices',
+            serviceType: 'servicePackServices',
+            hasLevel: 'Service Provider',
+            isPaasAdmin: true
+          },
+          {
+            name: 'User Licenses',
+            path: 'userServices',
+            angularComponent: 'groupServices',
+            hasLevel: 'Service Provider',
+            serviceType: 'userServices',
+            isPaasAdmin: true
+          },
+          {
+            name: 'Delete Group',
+            path: 'groupDelete',
+            angularComponent: 'groupDelete',
+            hasLevel: 'Service Provider',
+            serviceProviderPolicy: 'groupDelete',
+            isPaasAdmin: true
+          }
         ]
       }
-	]
-},
-{
-  label: 'REPORTS',
-  items: [
-{
-      name: 'Call Reports',
-      subMenus: groupReportRoutes.callReports
-    },
-  {
-      name: 'Utilization Reports',
-      subMenus: groupReportRoutes.utilizationReports
-    }
-]
-},
+    ]
+  },
+
   {
     label: 'Management',
     items: [
-        {
-          name: 'Announcements',
-          path: 'announcements',
-          angularComponent: 'groupAnnouncements',
-          version: '20'
-        },
-	      {
-          name: 'Call Processing Policy',
-          path:  'callProcessingPolicy' ,
-          angularComponent: 'groupCallProcessingPolicy',
-          hasLevel: 'Group'
-        },
-        {
-          name: 'Comm Barring',
-          path: 'comm-barring',
-          component: GroupCommunicationSettings
-        },
-        {
-          name: 'Feature Access Codes',
-          path:  'featureAccessCodes',
-          angularComponent: 'groupFeatureAccessCodes',
-          hasLevel: 'Group',
-          hasModuleRead: 'Group Feature Access Codes'
-        },
-        {
-          name: 'Group Web Policy',
-          path:  'groupWebPolicy',
-          component: GroupWebPolicy,
-          hasLevel: 'Service Provider',
-          isBreadcrumb: false
-        },
-        {
-          name: 'Intercept Group',
-          path: 'interceptGroup' ,
-          angularComponent: 'groupIntercept',
-          hasLevel: 'Group',
-          hasModuleRead: 'Intercept Group',
-          module:'Intercept Group'
-        },
-        {
-          name: 'Network Class of Service',
-          path:  'networkClassOfServices',
-          angularComponent: 'groupNetworkClassOfServices',
-          hasLevel: 'Group',
-          hasModuleRead: 'Network Class of Service'
-        },
-		{
-          name: 'Phone List And Directory',
-          path: 'phone-list-directory',
-          component: GroupPhoneListWithDirecotorySettings
-        },
-        {
-          name: 'Schedules',
-          path: 'schedules',
-		      angularComponent: 'groupSchedules',
-		      hasLevel: 'Group'
-        },
-        {
-          name: 'Viewable Packs',
-          path: 'viewablePacks',
-          angularComponent: 'groupViewablePacksIndex',
-          hasLevel: 'Group',
-          hasModuleRead: 'Viewable Service Packs'
-        },
-        {
-          name: 'VDM',
-          path:  'vdm',
-          angularComponent: 'vdmDashboard',
-          hasLevel: 'Group',
-          hasModuleRead: 'VDM'
-        }
+      {
+        name: 'Announcements',
+        path: 'announcements',
+        angularComponent: 'groupAnnouncements',
+        version: '20'
+      },
+      {
+        name: 'Call Processing Policy',
+        path: 'callProcessingPolicy',
+        angularComponent: 'groupCallProcessingPolicy',
+        hasLevel: 'Group'
+      },
+      {
+        name: 'Comm Barring',
+        path: 'comm-barring',
+        component: GroupCommunicationSettings
+      },
+      {
+        name: 'Feature Access Codes',
+        path: 'featureAccessCodes',
+        angularComponent: 'groupFeatureAccessCodes',
+        hasLevel: 'Group',
+        hasModuleRead: 'Group Feature Access Codes'
+      },
+      {
+        name: 'Group Web Policy',
+        path: 'groupWebPolicy',
+        component: GroupWebPolicy,
+        hasLevel: 'Service Provider',
+        isBreadcrumb: false
+      },
+      {
+        name: 'Intercept Group',
+        path: 'interceptGroup',
+        angularComponent: 'groupIntercept',
+        hasLevel: 'Group',
+        hasModuleRead: 'Intercept Group',
+        module: 'Intercept Group'
+      },
+      {
+        name: 'Network Class of Service',
+        path: 'networkClassOfServices',
+        angularComponent: 'groupNetworkClassOfServices',
+        hasLevel: 'Group',
+        hasModuleRead: 'Network Class of Service'
+      },
+      {
+        name: 'Phone List And Directory',
+        path: 'phone-list-directory',
+        component: GroupPhoneListWithDirecotorySettings
+      },
+      {
+        name: 'Schedules',
+        path: 'schedules',
+        angularComponent: 'groupSchedules',
+        hasLevel: 'Group'
+      },
+      {
+        name: 'Viewable Packs',
+        path: 'viewablePacks',
+        angularComponent: 'groupViewablePacksIndex',
+        hasLevel: 'Group',
+        hasModuleRead: 'Viewable Service Packs'
+      },
+      {
+        name: 'VDM',
+        path: 'vdm',
+        angularComponent: 'vdmDashboard',
+        hasLevel: 'Group',
+        hasModuleRead: 'VDM'
+      }
     ]
   }
 ]
