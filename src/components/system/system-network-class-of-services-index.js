d43a0131466edb5c73c8c43a075a90a008590c01
pankajs1306 <57499287+pankajs1306@users.noreply.github.com>
2020-05-27 15:01:48 +0530

    Full page view network class of services bug fixes

--- src/components/system/system-network-class-of-services-index.js
+++ src/components/system/system-network-class-of-services-index.js
@@ -2,19 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Switch, Route } from 'react-router-dom'
 import { AngularComponent } from '@/components/angular-component'
+import { UiCard } from '@/components/ui'
 
 export const SystemNetworkClassOfServicesIndex = ({ match }) => {
-
   const systemNetworkClassOfServiceRender = () => {
-    return  <AngularComponent component={'systemNetworkClassOfService'} />
+    return (
+      <UiCard title="">
+        <AngularComponent component={'systemNetworkClassOfService'} />
+      </UiCard>
+    )
   }
+
   return (
     <>
-    <Switch>
-      <Route path={`${match.path}/:name`} exact render={systemNetworkClassOfServiceRender} />
-      <Route render = {() => <AngularComponent component='systemNetworkClassOfServices' /> } />
-    </Switch>
-  </>
+      <Switch>
+        <Route
+          path={`${match.path}/:name`}
+          exact
+          render={systemNetworkClassOfServiceRender}
+        />
+        <Route
+          render={() => (
+            <AngularComponent component="systemNetworkClassOfServices" />
+          )}
+        />
+      </Switch>
+    </>
   )
 }
 
