58f6d880bceb81165246ae003a6e8cd9c240caad
pankajs1306 <57499287+pankajs1306@users.noreply.github.com>
2020-05-28 15:25:08 +0530

    commit file

---
+++ src/components/service-provider/service-provider-service-packs-index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Switch, Route } from 'react-router-dom'
+import { AngularComponent } from '@/components/angular-component'
+import { UiCard } from '@/components/ui'
+
+export const ServiceProviderServicePacksIndex = ({ match }) => {
+  const serviceProviderServicePackRender = () => {
+    return (
+      <UiCard title="">
+        <AngularComponent component={'serviceProviderServicePack'} />
+      </UiCard>
+    )
+  }
+
+  return (
+    <>
+      <Switch>
+        <Route
+          path={`${match.path}/:servicePackName`}
+          exact
+          render={serviceProviderServicePackRender}
+        />
+        <Route
+          render={() => (
+            <AngularComponent component="serviceProviderServicePacks" />
+          )}
+        />
+      </Switch>
+    </>
+  )
+}
+
+ServiceProviderServicePacksIndex.propTypes = {
+  history: PropTypes.object,
+  match: PropTypes.object
+}
