2014f6eba86ae57c41ec243eb1106fe070dedfe6
Johnny Cash <developer@parkbenchsolutions.com>
2020-05-28 22:52:17 -0400

    reverted code back to original

--- src/components/audits/audit.js
+++ src/components/audits/audit.js
@@ -5,7 +5,8 @@ import { useAlerts } from '@/store/alerts'
 import auditApi from '@/api/audits'
 import exportApi from '@/api/exports'
 import settingsApi from '@/api/settings'
-import { Input, Select } from 'rbx'
+import { AppBreadcrumb } from '@/components/app'
+import { Breadcrumb, Input, Select } from 'rbx'
 import {
   UiCard,
   UiButton,
@@ -35,6 +36,7 @@ const columns = [
 
 export const Audit = ({ history, match, isBreadcrumb = true }) => {
   const id = match.params.id
+  const formRef = React.useRef()
   const { alertDanger, alertSuccess } = useAlerts()
   const [showModal, setShowModal] = useState(false)
   const [showExportModal, setShowExportModal] = useState(false)
@@ -42,12 +44,17 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
   const [data, setData] = useState('')
   const [serviceType, setServiceType] = useState('')
   const [form, setForm] = useState({})
+  const [export2, setExport2] = useState({})
   const [endpoints, setEndpoints] = useState([])
+  const [formValid, setFormValid] = React.useState(false)
   const [showConfirm, setShowConfirm] = useState(false)
-  const [setExport2] = useState({})
   const KEY = 'exports'
 
   const { result, error, loading } = useAsync(() => auditApi.json(id), [id])
+
+  React.useEffect(() => {
+    if (formRef.current) setFormValid(formRef.current.checkValidity())
+  }, [form])
 
   if (error) alertDanger(error)
   const audit = result || {}
@@ -132,8 +139,9 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
           groupId: form.groupId
         }
       })
-      setExport2(iResult)
-      alertSuccess('Migration sent successfully to ' + form.endpoint)
+      setExport2('iResult', iResult)
+      console.log('iResuult')
+      alertSuccess('Export sent successfully to ' + form.endpoint)
     } catch (error_) {
       setShowExportModal(true)
       alertDanger(error_)
@@ -168,6 +176,13 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
 
   return (
     <>
+      {isBreadcrumb && (
+        <AppBreadcrumb>
+          <Breadcrumb.Item href="/#!/audits">Audits</Breadcrumb.Item>
+          <Breadcrumb.Item>Audit {id}</Breadcrumb.Item>
+        </AppBreadcrumb>
+      )}
+
       {loading ? (
         <UiLoadingCard />
       ) : (
@@ -208,7 +223,7 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
               columns={columns}
               rows={audit}
               rowKey="id"
-              pageSize={25}
+              pageSize={20}
               onClick={open}
             />
           </UiCard>
@@ -225,7 +240,7 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
         <UiLoadingModal isOpen={showLoading} />
       ) : (
         <UiCardModal
-          title={`Migration Audit ${form.groupId} (${form.id})`}
+          title={`Export Audit ${form.groupId} (${form.id})`}
           onCancel={() => setShowExportModal(false)}
           // onSave={uploadExport}
           onSave={
@@ -238,7 +253,7 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
               ? () => setShowConfirm(true)
               : null
           }
-          saveText="Migrate"
+          saveText="Export"
           isOpen={showExportModal}
         >
           <form>
@@ -381,7 +396,7 @@ export const Audit = ({ history, match, isBreadcrumb = true }) => {
         isOpen={showConfirm}
         onCancel={() => setShowConfirm(false)}
         onSave={uploadExport}
-        saveText="Migrate"
+        saveText="Export"
       >
         <blockquote>
           Are you sure you want to upload this audit {form.id} {form.groupId}?
